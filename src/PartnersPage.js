24e29428484e3efcda0fc95383ac93a6ed06c309
Raheedm <raheedmuzawar@gmail.com>
2024-02-27 01:24:51 +0530

    new sponsors

--- src/PartnersPage.js
+++ src/PartnersPage.js
@@ -3,12 +3,15 @@ import Partners from './Partners'
 import waffle from './assets/waffle.png'
 import softie from './assets/Softie1.JPEG'
 import shams from './assets/Shams.jpeg'
+import kaps from './assets/caps.png'
 
 function PartnersPage() {
     const sponsorsData = [
         { name: 'Sponsor 1', logo: waffle},
         { name: 'Sponsor 2', logo: softie },
         { name: 'Sponsor 3', logo: shams },
+        { name: 'Sponsor 3', logo: kaps },
+
 
         // Add more sponsors as needed
       ]
